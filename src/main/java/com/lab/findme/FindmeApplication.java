44adb1c5b7330f10ae27567ef2585cc7ebc5c62b
Ho9679 <103132530+Ho9679@users.noreply.github.com>
2022-09-17 01:49:26 +0900

    Initial commit

---
+++ src/main/java/com/lab/findme/FindmeApplication.java
@@ -0,0 +1,13 @@
+package com.lab.findme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab/findme/FindmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.findme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
